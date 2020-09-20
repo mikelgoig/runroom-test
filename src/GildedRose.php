d31f372ab3d7254c6166eebc68192138ed880725
Mikel Goig <m@mikelgoig.com>
2020-09-20 12:15:04 +0200

    🎨 Add type comparison

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -14,16 +14,16 @@ class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
                         $item->quality -= 1;
                     }
                 }
             } else {
                 if ($item->quality < 50) {
                     $item->quality += 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->sellIn < 11) {
                             if ($item->quality < 50) {
                                 $item->quality += 1;
@@ -38,15 +38,15 @@ class GildedRose
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
                 $item->sellIn -= 1;
             }
 
             if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->name !== 'Aged Brie') {
+                    if ($item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
                                 $item->quality -= 1;
                             }
                         }
